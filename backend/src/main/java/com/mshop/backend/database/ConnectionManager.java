c9606e6f88bb981b046430d8fa2228edbca00cd5
ezio19 <soso1486>
2016-06-15 00:08:19 +0100

    new changes : backend

---
+++ backend/src/main/java/com/mshop/backend/database/ConnectionManager.java
@@ -0,0 +1,67 @@
+package com.mshop.backend.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by Dell on 14/06/2016.
+ */
+public class ConnectionManager {
+
+    private static ConnectionManager instance = null;
+
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
+    private static final String CONN_STRING =
+            "jdbc:mysql://localhost/dbmobile?useSSL=false";
+
+
+    private Connection conn = null;
+
+    private ConnectionManager() {
+    }
+
+    public static ConnectionManager getInstance() {
+        if (instance == null) {
+            instance = new ConnectionManager();
+        }
+        return instance;
+    }
+
+
+    private boolean openConnection()
+    {
+        try {
+            conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            return true;
+        }
+        catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        }
+
+    }
+
+    public Connection getConnection()
+    {
+        if (conn == null) {
+            if (openConnection()) {
+                System.out.println("Connection opened");
+                return conn;
+            } else {
+                return null;
+            }
+        }
+        return conn;
+    }
+
+    public void close() {
+        System.out.println("Closing connection");
+        try {
+            conn.close();
+            conn = null;
+        } catch (Exception e) {
+        }
+    }
+}
--- backend/src/main/java/com/mshop/backend/database/DataBaseService.java
+++ backend/src/main/java/com/mshop/backend/database/DataBaseService.java
@@ -4,6 +4,8 @@ import com.mshop.backend.model.Product;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,27 +15,37 @@ import java.util.List;
  */
 public class DataBaseService {
 
-    public static final String CLASS_NAME = "com.mysql.jdbc.Driver";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "password";
-    private static final String CONN_STRING = "jdbc:mysql://localhost/dbmobile?useSSL=false";
+    private static Connection conn = ConnectionManager.getInstance().getConnection();
 
-    public Connection connecter()   {
+    public List<Product> getProducts(String density, String category)  {
 
-        Connection con = null;
+        List<Product> products = new ArrayList<>();
+        PreparedStatement pst = null;
+        String query = "select prods.* , i.* from images i join (select p.* from products p where p.category = ?) As prods where i.product_name = prods.name and i.density = ?";
+
         try {
-            Class.forName(CLASS_NAME);
-            con = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+            pst = conn.prepareStatement(query);
+            pst.setString(1,category);
+            pst.setString(2,density);
+
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.getRow() == 0) {
+                return products;
+            }else{
+                rs.beforeFirst();
+                while (rs.next()) {
+
+                    Product product = new Product();
+                }
+            }
+
+
+        }catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return con;
-    }
 
-    public List<Product> getBooks(String density)  {
         return new ArrayList<>();
     }
 }
